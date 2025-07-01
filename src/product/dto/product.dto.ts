ff22d17e29f443cd89c3ed372b6e1a469daa69dd
Sergey Lem <sergey.lem@gmail.com>
2025-07-01 20:46:40 +0500

    Feat: Add product CRUD operations

---
+++ src/product/dto/product.dto.ts
@@ -0,0 +1,40 @@
+import { ArrayMinSize, IsNotEmpty, IsNumber, IsString } from 'class-validator'
+
+export class ProductDto {
+	@IsString({
+		message: 'Title is required'
+	})
+	@IsNotEmpty({ message: 'Title cannot be empty' })
+	title: string
+
+	@IsString({ message: 'Description is required' })
+	@IsNotEmpty({ message: 'Description cannot be empty' })
+	description: string
+
+	@IsNumber({}, { message: 'Price must be a number' })
+	@IsNotEmpty({ message: 'Price cannot be empty' })
+	price: number
+
+	@IsString({
+		message: 'Specify at least one image',
+		each: true
+	})
+	@ArrayMinSize(1, { message: 'At least one image is required' })
+	@IsNotEmpty({
+		each: true,
+		message: 'Image path cannot be empty'
+	})
+	images: string[]
+
+	@IsString({
+		message: 'Category is required'
+	})
+	@IsNotEmpty({ message: 'Category ID cannot be empty' })
+	categoryId: string
+
+	@IsString({
+		message: 'Color is required'
+	})
+	@IsNotEmpty({ message: 'Color ID cannot be empty' })
+	colorId: string
+}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
-import { ProductController } from './product.controller';
+import { Module } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { ProductController } from './product.controller'
+import { ProductService } from './product.service'
 
 @Module({
-  controllers: [ProductController],
-  providers: [ProductService],
+	controllers: [ProductController],
+	providers: [ProductService, PrismaService]
 })
 export class ProductModule {}
